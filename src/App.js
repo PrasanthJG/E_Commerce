29ad4e115a2d3623fcbf79ea102f957e9f301af0
PrasanthJG <prasanthjg56@gmail.com>
2024-07-26 09:37:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './page/Navbar';
+import Sidebar from './page/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
